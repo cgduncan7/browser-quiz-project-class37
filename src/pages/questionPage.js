c82537963e2b3d43f2ac59e0f5679186b65ecad0
tonyclem <tonyclem001@gmail.com>
2022-05-13 07:38:29 +0200

    Fix the bugs on the eventListener button, if user don't select a question don't go to the questions

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -27,10 +27,6 @@ export const initQuestionPage = () => {
     answerElement.addEventListener('click', chooseAnswer);
     answersListElement.appendChild(answerElement);
   }
-
-  document
-    .getElementById(NEXT_QUESTION_BUTTON_ID)
-    .addEventListener('click', nextQuestion);
 };
 
 function chooseAnswer() {
@@ -61,6 +57,11 @@ function chooseAnswer() {
   answerElements.forEach((element) => {
     element.removeEventListener("click", chooseAnswer);
   });
+
+  // EventLister for the next button
+  document
+    .getElementById(NEXT_QUESTION_BUTTON_ID)
+    .addEventListener('click', nextQuestion);
 }
 
 // increment
@@ -69,6 +70,7 @@ const incrementScore = (point) => {
   document.getElementById(SCORE_DIV_ID).innerText = quizData.score;
 };
 
+// EventLister Function that executed to the next Question
 const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
 
